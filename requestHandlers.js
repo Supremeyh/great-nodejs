cd9ad53093cc033ea00ef2e8ea2df611c582e963
sea <yanghai@yuntongxun.com>
2018-10-01 11:07:02 +0800

    10.1 onRequset

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,36 @@
-var exec = require('child_process').exec;
+// var exec = require('child_process').exec;
 
 function start(res) {
     console.log('start')
 
-    exec("find /", 
-    { timeout: 10000, maxBuffer: 20000 * 1024},
-    function(error,stdout,stderr){
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.write(stdout);
-        res.end();
-    })
+    var body = `
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>Document</title>
+            </head>
+            <body>
+                <form action="/upload" method="post">
+                    <textarea name="text" id="text" cols="30" rows="10"></textarea>
+                    <input type="submit" value="Submit File"/>
+                </form>
+            </body>
+        </html>
+    `
+
+     // exec("find /", 
+    // { timeout: 10000, maxBuffer: 20000 * 1024},
+    // function(error,stdout,stderr){
+    //     res.writeHead(200, {"Content-Type": "text/plain"});
+    //     res.write(stdout);
+    //     res.end();
+    // })
+
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(body);
+    res.end();
 
 }
 
--- server.js
+++ server.js
@@ -3,17 +3,19 @@ var url = require("url");
 
 
 function start(route, handle) {
+    function onRequest (request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log('req for '+ pathname + 'is received.');
+
+        route(handle, pathname, response)
+
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // response.write(content);
+        // response.end();
+    }
+
     http
-        .createServer(function(req, res){
-            var pathname = url.parse(req.url).pathname;
-            console.log('req for '+ pathname + 'is received.');
-
-            route(handle, pathname, res)
-
-            // res.writeHead(200, {"Content-Type": "text/plain"});
-            // res.write(content);
-            // res.end();
-        })
+        .createServer(onRequest)
         .listen(8888);
 
         console.log('server started');
